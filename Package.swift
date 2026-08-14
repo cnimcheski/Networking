3cfd9d337758ee6fe27c35bcf32f2a65810780d7
cnimcheski <cnimcheski@hotmail.com>
2026-08-13 18:18:58 -0700

    Renamed package too Networking

--- Package.swift
+++ Package.swift
@@ -4,26 +4,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "APIManager",
+    name: "Networking",
     platforms: [
         .iOS(.v17)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "APIManager",
-            targets: ["APIManager"]
+            name: "Networking",
+            targets: ["Networking"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "APIManager"
-        ),
-        .testTarget(
-            name: "APIManagerTests",
-            dependencies: ["APIManager"]
+            name: "Networking"
         ),
     ]
 )
