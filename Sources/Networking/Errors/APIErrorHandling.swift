e56181acf8e7e78e87ccd0e578e32534ac582347
cnimcheski <cnimcheski@hotmail.com>
2026-08-15 15:28:23 -0700

    Got rid of Sendable conformance in APIErrorHandling

--- Sources/Networking/Errors/APIErrorHandling.swift
+++ Sources/Networking/Errors/APIErrorHandling.swift
@@ -9,7 +9,7 @@ import Foundation
 
 /// Allows apps to provide an action for how global API errors should be handled.
 /// Ex: Toasts or Alerts.
-public protocol APIErrorHandling<APIGlobalError>: Sendable {
+public protocol APIErrorHandling<APIGlobalError> {
     associatedtype APIGlobalError: APIError
     
     /// Handles an API-level error and optionally retries the failed request.
