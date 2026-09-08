cf015278055b382514caf192711d55f76fc7eb31
cnimcheski <cnimcheski@hotmail.com>
2026-09-07 21:00:15 -0700

    Allowed for returning data without using jsondecoder

--- Sources/Networking/NetworkingClient.swift
+++ Sources/Networking/NetworkingClient.swift
@@ -27,6 +27,7 @@ public final class NetworkingClient {
         try errorHandler.parseError(from: response, with: data)
         try response.checkForServerError()
         try response.checkAuthStatus()
+        if let data = data as? T { return data }
         return try endpoint.jsonDecoder().decode(T.self, from: data)
     }
     
@@ -40,6 +41,7 @@ public final class NetworkingClient {
         try endpoint.parseError(from: response, with: data)
         try response.checkForServerError()
         try response.checkAuthStatus()
+        if let data = data as? T { return data }
         return try endpoint.jsonDecoder().decode(T.self, from: data)
     }
 }
